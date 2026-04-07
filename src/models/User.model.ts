e96356e165c18d55a0688f0dd8d89cc44ea7cc0f
Nadim Nahle <nahlenadim@gmail.com>
2026-04-07 20:07:32 +0300

    Create User Interface

---
+++ src/models/User.model.ts
@@ -0,0 +1,50 @@
+export interface User {
+  id: string;
+  email: string;
+  password: string;
+  firstName: string;
+  lastName: string;
+  createdAt: string;
+  updatedAt: string;
+}
+ 
+export interface UserCreateDto {
+  email: string;
+  password: string;
+  firstName: string;
+  lastName: string;
+}
+ 
+export interface UserUpdateDto {
+  email?: string;
+  password?: string;
+  firstName?: string;
+  lastName?: string;
+}
+ 
+export interface UserResponseDto {
+  id: string;
+  email: string;
+  firstName: string;
+  lastName: string;
+  createdAt: string;
+  updatedAt: string;
+}
+ 
+export interface LoginDto {
+  email: string;
+  password: string;
+}
+ 
+export interface AuthResponse {
+  token: string;
+  user: UserResponseDto;
+}
+ 
+export interface PaginatedResponse<T> {
+  data: T[];
+  total: number;
+  page: number;
+  limit: number;
+  totalPages: number;
+}
